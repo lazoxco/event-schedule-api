b22e83454718a021f51331418673141676dd06a6
github username <github email address>
2021-06-21 22:27:40 -0400

    Seeded DB.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+keynotes = Keynote.create([
+  {title: "What is HTML?", speaker: "Captain America", description: "Learn the basics of HTML", date_time: "06/12/2022 9:00 AM"},
+  {title: "What is CSS?", speaker: "Black Widow", description: "Learn the basics of CSS", date_time: "06/12/2022 10:00 AM"},
+  {title: "What is JavaScript?", speaker: "Tony Stark", description: "Learn the basics of JavaScript", date_time: "06/12/2022 11:00 AM"}
+])
